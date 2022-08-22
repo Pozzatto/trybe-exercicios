34434d8323a5e36f6b1d5a96d6b13f31bcd86ef3
Vinicius <vpozzatto@gmail.com>
2022-08-22 17:34:14 -0300

    exercicios/4.3

--- fundamentos/bloco-04-Introducao-a-JavaScript-e-logica-de-AprogramacaDo/dia-03-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-04-Introducao-a-JavaScript-e-logica-de-AprogramacaDo/dia-03-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -49,3 +49,13 @@
 //----------------------------------------------------------
 //Exercicio 4
 
+let n = 4;
+let linha = '';
+let asterisco = '*';
+
+for(let index=0; index<n; index+=1){
+    linha += asterisco;
+}
+for (let index=0; index<n; index+=1) {
+    console.log(linha)    
+}
