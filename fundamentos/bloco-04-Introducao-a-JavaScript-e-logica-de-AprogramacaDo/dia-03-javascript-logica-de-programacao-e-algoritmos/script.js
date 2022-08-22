674c914840d36462e0b7b0c2462ed170bcf3b5fc
Vinicius <vpozzatto@gmail.com>
2022-08-22 12:27:58 -0300

    exercicios/4.3

---
+++ fundamentos/bloco-04-Introducao-a-JavaScript-e-logica-de-AprogramacaDo/dia-03-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -0,0 +1,51 @@
+//----------------------------------------------------------
+//Exercicio 1
+
+// let n = 10;
+// let resultado = 1;
+
+// for(let i=1;i<=n;i+=1){
+//   resultado *= i
+// }
+// console.log(resultado);
+
+//----------------------------------------------------------
+//Exercicio 2
+
+// const nome = 'trybe';
+// let resultado = '';
+
+// for (let i = nome.length -1; i >= 0; i --) {
+//   resultado += nome[i];
+// }
+// console.log(resultado);
+
+//----------------------------------------------------------
+//Exercicio 3
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let resultado = array[0];
+
+// for(let index = 1; index < array.length; index += 1){
+//   if(resultado.length < array[index].length){
+//     resultado = array[index];
+//   }
+// }
+// console.log(resultado);
+
+//----------------------------------------------------------
+//Exercicio 3
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let resultado = array[0];
+
+// for(let index = 1; index < array.length; index += 1){
+//   if(resultado.length > array[index].length){
+//     resultado = array[index];
+//   }
+// }
+// console.log(resultado);
+
+//----------------------------------------------------------
+//Exercicio 4
+
