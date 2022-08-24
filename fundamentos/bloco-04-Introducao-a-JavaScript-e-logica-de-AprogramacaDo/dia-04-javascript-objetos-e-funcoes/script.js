cb3111076742a7e7106a356eb0786aa10aa403b6
Vinicius <vpozzatto@gmail.com>
2022-08-23 23:42:53 -0300

    exercicios/4.4

--- fundamentos/bloco-04-Introducao-a-JavaScript-e-logica-de-AprogramacaDo/dia-04-javascript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-04-Introducao-a-JavaScript-e-logica-de-AprogramacaDo/dia-04-javascript-objetos-e-funcoes/script.js
@@ -1,15 +1,51 @@
-let player = {
-name : 'Marta',
-lastName : 'Silva',
-age : 34,
-medals : { golden: 2, silver: 3 }
-}
-player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
+//---------------------------------------------------------
+//Conteudo
+
+// let player = {
+// name : 'Marta',
+// lastName : 'Silva',
+// age : 34,
+// medals : { golden: 2, silver: 3 }
+// }
+// player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
 
 
-console.table(player);
-console.log(player.bestInTheWorld);
-console.log('A jogador ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.');
-console.log('A jogador ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes.');
-console.log(player.medals);
-console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.')
+// console.table(player);
+// console.log(player.bestInTheWorld);
+// console.log('A jogador ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.');
+// console.log('A jogador ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes.');
+// console.log(player.medals);
+// console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.')
+
+// //---------------------------------------------------------
+// //Conteudo
+
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge',
+//   };
+//   for(let index in names){
+//     console.log('Olá ' + names[index]);
+//   }
+
+//---------------------------------------------------------
+//Conteudo
+
+// let car = {
+//     model: 'A3 Sedan',
+//     manufacturer: 'Audi',
+//     year: 2020
+//   };
+//   for(let index in car){
+//     console.log(index, car[index]);
+//   };
+
+//---------------------------------------------------------
+//Conteudo
+
+// function greetingsMessage(name) {
+//     greetingsMessage ('Vinicius')
+//     console.log("Boas vindas, " + name);
+//   }
+  
