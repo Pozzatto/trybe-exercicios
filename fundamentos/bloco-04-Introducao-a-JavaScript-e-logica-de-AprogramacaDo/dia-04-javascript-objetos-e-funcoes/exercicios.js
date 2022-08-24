fd1dce83bb3fa332d6f45a92e9eb267e980bdd0a
Vinicius <vpozzatto@gmail.com>
2022-08-24 00:17:36 -0300

    atualizacao

--- fundamentos/bloco-04-Introducao-a-JavaScript-e-logica-de-AprogramacaDo/dia-04-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-04-Introducao-a-JavaScript-e-logica-de-AprogramacaDo/dia-04-javascript-objetos-e-funcoes/exercicios.js
@@ -89,3 +89,9 @@
 
 // console.log(leitor.livrosFavoritos);
 // console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos.');
+
+let a = "Serei a melhor pessoa dev";
+let b = "Esse é meu jeito dev de ser!";
+let num = 2;
+let strings = [a, b];
+console.log((typeof strings.length) == (typeof num));
