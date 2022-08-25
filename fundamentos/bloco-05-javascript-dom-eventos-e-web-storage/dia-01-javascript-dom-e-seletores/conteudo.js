5461fbb5f6268f2f7a04f47cfe9311742842a06a
Vinicius <vpozzatto@gmail.com>
2022-08-25 15:41:10 -0300

    conteudo

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/conteudo.js
@@ -0,0 +1,15 @@
+document.getElementById('header-container').style.backgroundColor = '#06d6a0';
+
+document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = 'pink';
+
+document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = 'yellow';
+
+document.getElementsByTagName('h3')[0].style.backgroundColor = 'purple';
+
+document.getElementsByTagName('h3')[1].style.backgroundColor = 'purple';
+
+document.getElementsByTagName('h3')[2].style.backgroundColor = 'black';
+
+document.getElementsByTagName('h3')[3].style.backgroundColor = 'black';
+
+document.querySelector('#footer-container').style.backgroundColor = '#001845';
