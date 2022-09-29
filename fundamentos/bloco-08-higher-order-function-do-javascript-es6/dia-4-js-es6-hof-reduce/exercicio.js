7c92cd7b59e977cb782e4080b5fa74256271f03e
Vinicius <vpozzatto@gmail.com>
2022-09-29 12:11:22 -0300

    exercicios/8.4

--- fundamentos/bloco-08-higher-order-function-do-javascript-es6/dia-4-js-es6-hof-reduce/exercicio.js
+++ fundamentos/bloco-08-higher-order-function-do-javascript-es6/dia-4-js-es6-hof-reduce/exercicio.js
@@ -1,84 +1,110 @@
-const estudantes = [
-    {
-      nome: 'Jorge',
-      sobrenome: 'Silva',
-      idade: 14,
-      turno: 'Manhã',
-      materias: [
-        { name: 'Matemática', nota: 67 },
-        { name: 'Português', nota: 79 },
-        { name: 'Química', nota: 70 },
-        { name: 'Biologia', nota: 65 },
-      ],
-    },
-    {
-      nome: 'Mario',
-      sobrenome: 'Ferreira',
-      idade: 15,
-      turno: 'Tarde',
-      materias: [
-        { name: 'Matemática', nota: 59 },
-        { name: 'Português', nota: 80 },
-        { name: 'Química', nota: 78 },
-        { name: 'Biologia', nota: 92 },
-      ],
-    },
-    {
-      nome: 'Jorge',
-      sobrenome: 'Santos',
-      idade: 15,
-      turno: 'Manhã',
-      materias: [
-        { name: 'Matemática', nota: 76 },
-        { name: 'Português', nota: 90 },
-        { name: 'Química', nota: 70 },
-        { name: 'Biologia', nota: 80 },
-      ],
-    },
-    {
-      nome: 'Maria',
-      sobrenome: 'Silveira',
-      idade: 14,
-      turno: 'Manhã',
-      materias: [
-        { name: 'Matemática', nota: 91 },
-        { name: 'Português', nota: 85 },
-        { name: 'Química', nota: 92 },
-        { name: 'Biologia', nota: 90 },
-      ],
-    },
-    {
-      nome: 'Natalia',
-      sobrenome: 'Castro',
-      idade: 14,
-      turno: 'Manhã',
-      materias: [
-        { name: 'Matemática', nota: 70 },
-        { name: 'Português', nota: 70 },
-        { name: 'Química', nota: 60 },
-        { name: 'Biologia', nota: 50 },
-      ],
-    },
-    {
-      nome: 'Wilson',
-      sobrenome: 'Martins',
-      idade: 14,
-      turno: 'Manhã',
-      materias: [
-        { name: 'Matemática', nota: 80 },
-        { name: 'Português', nota: 82 },
-        { name: 'Química', nota: 79 },
-        { name: 'Biologia', nota: 75 },
-      ],
-    },
-  ];
+// const arrays = [
+//   ['1', '2', '3'],
+//   [true],
+//   [4, 5, 6],
+// ];
 
-//   const getBestClass = (acc, curr) => acc.nota > curr.nota ? acc : curr;
+// const flatten = () => arrays.reduce((acc, cur) => acc.concat(cur), []);
+// console.log(flatten());
 
-const reportBetter = (students) => students.map((student) => ({
-  name: student.nome,
-  materia: student.materias
-    .reduce((acc, curr) => acc.nota > curr.nota ? acc : curr).name,
-}));
+//---------
 
-console.log(reportBetter(estudantes));
+// const books = [
+//   {
+//     id: 1,
+//     name: 'As Crônicas de Gelo e Fogo',
+//     genre: 'Fantasia',
+//     author: {
+//       name: 'George R. R. Martin',
+//       birthYear: 1948,
+//     },
+//     releaseYear: 1991,
+//   },
+//   {
+//     id: 2,
+//     name: 'O Senhor dos Anéis',
+//     genre: 'Fantasia',
+//     author: {
+//       name: 'J. R. R. Tolkien',
+//       birthYear: 1892,
+//     },
+//     releaseYear: 1954,
+//   },
+//   {
+//     id: 3,
+//     name: 'Fundação',
+//     genre: 'Ficção Científica',
+//     author: {
+//       name: 'Isaac Asimov',
+//       birthYear: 1920,
+//     },
+//     releaseYear: 1951,
+//   },
+//   {
+//     id: 4,
+//     name: 'Duna',
+//     genre: 'Ficção Científica',
+//     author: {
+//       name: 'Frank Herbert',
+//       birthYear: 1920,
+//     },
+//     releaseYear: 1965,
+//   },
+//   {
+//     id: 5,
+//     name: 'A Coisa',
+//     genre: 'Terror',
+//     author: {
+//       name: 'Stephen King',
+//       birthYear: 1947,
+//     },
+//     releaseYear: 1986,
+//   },
+//   {
+//     id: 6,
+//     name: 'O Chamado de Cthulhu',
+//     genre: 'Terror',
+//     author: {
+//       name: 'H. P. Lovecraft',
+//       birthYear: 1890,
+//     },
+//     releaseYear: 1928,
+//   },
+// ];
+
+// const reduceNames = () => books.reduce((acc, cur) => `${acc} ${cur.author.name}.`, '');
+// console.log(reduceNames());
+
+// const averageAge = () => books.reduce((acc, cur) => 
+//   (acc + (cur.releaseYear - cur.author.birthYear)), 0) / books.length;
+// console.log(averageAge());
+
+// const longestNamedBook = () => books.reduce((acc, cur) => 
+//   (acc.name.length > cur.name.length) ? acc : acc = cur);
+// console.log(longestNamedBook());
+
+// const names = [
+//   'Aanemarie', 'Adervandes', 'Akifusa',
+//   'Abegildo', 'Adicellia', 'Aladonata',
+//   'Abeladerco', 'Adieidy', 'Alarucha',
+// ];
+
+// const containsA = () => {
+//   let result = 0;
+//   names.forEach((element) => {
+//     result += element.split('').reduce((acc, cur) => (cur === 'a' || cur === 'A') ? acc + 1 : acc, 0)
+//   })
+//   return result;
+// };
+// console.log(containsA());
+
+//------------
+
+// const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+// const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+// const studentAverage = () => students.map((element, index) => ({
+//   name: element, 
+//   avarage: grades[index].reduce((acc, cur) => acc + cur, 0)}));
+// studentAverage()
+// console.log(studentAverage())
